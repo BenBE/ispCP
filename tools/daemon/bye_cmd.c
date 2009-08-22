d3584285fc612902fbdf87677374786be3caab4a
Benedikt Heintel <benedikt.heintel@isp-control.net>
2009-08-22 10:11:36 +0000

    * Preparation of Re-Release ispCP ω Omega 1.0.2

---
+++ tools/daemon/bye_cmd.c
@@ -0,0 +1,7 @@
+
+#include "bye_cmd.h"
+
+int bye_cmd(int fd, char *msg)
+{
+	return bye_syntax(fd, msg);
+}
---
+++ tools/daemon/lr_cmd.c
@@ -0,0 +1,8 @@
+
+#include "lr_cmd.h"
+
+int lr_cmd(int fd, char *msg)
+{
+	return lr_syntax(fd, msg);
+}
+
