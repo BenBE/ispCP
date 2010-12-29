a9c5ced0f19b358145285b50187e01f0ddc8991d
Benedikt Heintel <benedikt.heintel@isp-control.net>
2010-12-29 22:47:48 +0000

    * Reverted Typo

--- engine/ispcp_common_methods.pl
+++ engine/ispcp_common_methods.pl
@@ -13,7 +13,7 @@
 #
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-# License for the specimfic language governing rights and limitations
+# License for the specific language governing rights and limitations
 # under the License.
 #
 # The Original Code is "VHCS - Virtual Hosting Control System".
